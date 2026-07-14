608db631a06c38cd7b738451d30a57f718fd2f77
Zack M. Davis <code@zackmdavis.net>
2026-07-14 10:10:35 -0700

    more theme matching (feat. Claude Code)

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,6 +3,7 @@
 
 AUTHOR = 'Zack M. Davis'
 SITENAME = 'An Algorithmic Lucidity'
+SITESUBTITLE = 'a blog'
 SITEURL = ''
 
 PATH = 'content'
@@ -20,7 +21,28 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = ()
+LINKS = (
+    ('Beach City Bugle', 'http://www.beachcitybugle.com/'),
+    ('Claude Opus 4.5', 'https://claudeopus45.substack.com/'),
+    ('Compass Rose', 'http://benjaminrosshoffman.com/'),
+    ('Daniel Lemire', 'http://lemire.me/blog/'),
+    ('David R. MacIver', 'http://www.drmaciver.com/blog/'),
+    ('Equestria Daily', 'http://www.equestriadaily.com/'),
+    ('From Automorphy to Electricity', 'http://automorphy.blogspot.com/'),
+    ('Gallabytes', 'http://gallabytes.com/archive.html'),
+    ('Jeff Kaufman', 'http://www.jefftk.com/index'),
+    ('Julia Evans', 'http://jvns.ca/'),
+    ('Meteuphoric', 'http://meteuphoric.wordpress.com/'),
+    ('Otium', 'http://srconstantin.wordpress.com/'),
+    ('Reflective Disequilibrium', 'http://reflectivedisequilibrium.blogspot.com/'),
+    ('Ribbonfarm', 'http://www.ribbonfarm.com/'),
+    ('Seeking Whence', 'http://grognor.blogspot.com/'),
+    ('Slate Star Codex', 'http://slatestarcodex.com/'),
+    ('Stuart Spence Stuff', 'http://stuartspencestuff.blogspot.com/'),
+    ('The Rationalist Conspiracy', 'http://rationalconspiracy.com/'),
+    ("The Story's Story", 'http://jseliger.wordpress.com/'),
+    ('The Unit of Caring', 'http://theunitofcaring.tumblr.com/'),
+)
 
 # Social widget
 SOCIAL = ()
